0d422b8e67102d760f2b7cd28b1e4993e1cd8aa8
Vitor Pamplona <vitor-pamplona@live.com>
2023-06-06 22:52:01 -0300

    Tela cadastro de usuário criada

--- app/lib/cadastro_usuario.dart
+++ app/lib/cadastro_usuario.dart
@@ -1,3 +1,105 @@
-class CadastroUsuario extends {
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cadastro de Usuário',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: RegistrationScreen(),
+    );
+  }
+}
+
+class RegistrationScreen extends StatefulWidget {
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro de Usuário'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Nome',
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Senha',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: () {
+                // Lógica de cadastro aqui
+                String name = _nameController.text;
+                String email = _emailController.text;
+                String password = _passwordController.text;
+
+                // Exemplo de exibição dos dados cadastrados
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Dados de Cadastro'),
+                      content: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text('Nome: $name'),
+                          SizedBox(height: 8.0),
+                          Text('Email: $email'),
+                          SizedBox(height: 8.0),
+                          Text('Senha: $password'),
+                        ],
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Fechar'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text('Cadastrar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- app/lib/fidelio.dart
+++ app/lib/fidelio.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.lightGreen,
+          backgroundColor: Colors.deepOrange,
           title: Text(widget.title),
         ),
         body: Center(
