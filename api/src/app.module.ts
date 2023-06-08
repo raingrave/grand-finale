f236083d4e72dace9d8eb16bb8bab66008e64bc5
Luis Fernando <luis.phpdev@gmail.com>
2023-06-08 14:18:44 -0300

    adicionado endpoint de usuários

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PhysicalActivitieModule } from './physical-activitie/physical-activitie.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AppModule, PhysicalActivitieModule],
+  imports: [AppModule, PhysicalActivitieModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/src/users/users.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma-service';
+
+@Injectable()
+export class UsersService {
+
+  constructor(private prisma: PrismaService) {
+    this.prisma = prisma;
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: createUserDto
+    })
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.user.findUnique({
+      where: {
+        id: id
+      }
+    })
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      data: updateUserDto,
+      where: {
+        id: id
+      }
+    })
+  }
+
+  remove(id: number) {
+    return this.prisma.user.delete({
+      where: {
+        id: id
+      }
+    })
+  }
+}
